fdceddaad57527a40b36124ddb4f245d41c54b0f
ykfgod <ykfgodnew@gmail.com>
2018-11-01 22:58:40 +0500

    1st try

--- Laba1/new.py
+++ Laba1/new.py
@@ -1,2 +1,12 @@
-new line
-and another line
+text = "qwer tyuio"
+a = len(text) # Определяем количество символов в строке
+i = 0
+zam = ''
+while i <= a-2:
+    k = text[i+1]
+    text.replace(text[i+1], text[i])
+    text.replace(text[i], k)
+    i = i+1
+
+print(text)
+
